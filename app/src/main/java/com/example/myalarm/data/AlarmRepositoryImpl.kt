1fb0d41a16b8488aebf46b669efe8bc304633b8e
Yakov <yakha163@gmail.com>
2023-07-07 17:45:44 +0700

    Create fun generateQuestion

--- app/src/main/java/com/example/myalarm/data/AlarmRepositoryImpl.kt
+++ app/src/main/java/com/example/myalarm/data/AlarmRepositoryImpl.kt
@@ -5,6 +5,7 @@ import com.example.myalarm.domain.enteties.Level
 import com.example.myalarm.domain.enteties.Question
 import com.example.myalarm.domain.enteties.QuestionSetting
 import com.example.myalarm.domain.repository.AlarmRepository
+import kotlin.random.Random
 
 class AlarmRepositoryImpl(
     private val alarmDao: AlarmDao
@@ -43,6 +44,44 @@ class AlarmRepositoryImpl(
     }
 
     override fun generateQuestion(level: Level): Question {
-        TODO("Not yet implemented")
+        val questionSetting = getQuestionSettings(level)
+
+        while(true) {
+
+            val num1 = Random.nextInt(1, questionSetting.maxValueNum1)
+            val num2 = Random.nextInt(questionSetting.maxValueNum2)
+            val num3 = Random.nextInt(questionSetting.maxValueNum3)
+            val action1 = actionExample1.random()
+            val action2 = actionExample2.random()
+
+            return if(action1 == "/"){
+                if(divTest(num1, num2)){
+                    val numTemp = doAction(num1, num2, action1)
+                    val answer= doAction(numTemp, num3, action2)
+
+                    Question(num1, num2, num3, answer, action1, action2)
+                } else {
+                    continue
+                }
+            } else {
+                val numTemp = doAction(num1, num2, action1)
+                val answer= doAction(numTemp, num3, action2)
+
+                Question(num1, num2, num3,answer, action1, action2)
+            }
+
+        }
     }
+
+    private fun doAction(num1: Int, num2: Int, action1: String): Int{
+      return when(action1){
+            "+" -> num1 + num2
+            "-" -> num1 - num2
+            "*" -> num1 * num2
+            "/" -> num1 / num2
+          else -> throw RuntimeException("Unknown character $action1")
+        }
+    }
+
+    private fun divTest(num1: Int, num2: Int) = num1 % num2 == 0
 }
